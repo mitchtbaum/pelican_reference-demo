e758352e019bce79bc7c5d3397ce8621c11f3b5f
Mitchell Tannenbaum <mitchtbaum@gmail.com>
2014-11-04 11:48:18 -0500

    Add reference plugin setting.

--- settingsfile.py
+++ settingsfile.py
@@ -5,4 +5,5 @@ CATEGORY_URL = "{slug}/"
 CATEGORY_SAVE_AS = "{slug}/index.html"
 ARTICLE_URL = "{category}/{slug}/"
 ARTICLE_SAVE_AS = "{category}/{slug}/index.html"
-
+PLUGIN_PATH = 'pelican-plugins'
+PLUGINS = ['pelican_reference']
