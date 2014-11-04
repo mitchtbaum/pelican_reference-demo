d52a785e9165d126dd8cf7ed2369d0827e6e2ce3
Mitchell Tannenbaum <mitchtbaum@gmail.com>
2014-11-04 12:40:21 -0500

    holding

--- content/track/Ob-La-Di,_Ob-La-Da.md
+++ content/track/Ob-La-Di,_Ob-La-Da.md
@@ -1,5 +1,5 @@
 Title: Ob-La-Di, Ob-La-Da
-Album: the-white-album
+Album: white-album
 
 "Ob-La-Di, Ob-La-Da" is a song credited to Lennon–McCartney, but written by Paul McCartney and released by the Beatles on their 1968 album The Beatles (commonly called The White Album). It was released as a single that same year in many countries, but not in the United Kingdom, nor in the United States until 1976.
 http://en.wikipedia.org/wiki/Ob-La-Di,_Ob-La-Da
--- settingsfile.py
+++ settingsfile.py
@@ -1,10 +1,13 @@
 SITENAME = "pelican_reference demo"
-SITEURL = "http://mitchtbaum.github.io/pelican_reference-demo/"
+#SITEURL = "http://mitchtbaum.github.io/pelican_reference-demo/"
+SITEURL = "http://localhost:8000"
 DEFAULT_DATE = "fs"
 CATEGORY_URL = "{slug}/"
 CATEGORY_SAVE_AS = "{slug}/index.html"
 ARTICLE_URL = "{category}/{slug}/"
 ARTICLE_SAVE_AS = "{category}/{slug}/index.html"
-PLUGIN_PATH = 'pelican-plugins'
+PLUGIN_PATHS = 'plugins'
 PLUGINS = ['pelican_reference']
-SLUGIFY_SOURCE = 'basename'
+#SLUGIFY_SOURCE = 'basename'
+PATH = "content"
+THEME = "theme"
